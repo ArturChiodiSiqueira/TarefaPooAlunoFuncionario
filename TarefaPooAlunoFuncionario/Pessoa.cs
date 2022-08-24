7ad6c8e90dbbb3b5ec6775d60f883a28f18f2578
artur <artur@DESKTOP-V0JU5PP>
2022-08-24 16:31:01 -0300

    Add project files.

---
+++ TarefaPooAlunoFuncionario/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaPooAlunoFuncionario
+{
+    internal class Pessoa
+    {
+        String Nome;
+        DateTime DataNascimento;
+        long Cpf;
+    }
+}
